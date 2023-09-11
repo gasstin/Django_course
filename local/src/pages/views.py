1f8e78d1d8c3a86e4da398f8d0ef093913aa9954
gasstin <gastonl1994@gmail.com>
2023-09-11 11:22:32 -0300

    adding templates

--- local/src/pages/views.py
+++ local/src/pages/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello</h1>")
-    return render(request, 'home.html', {})
+    my_context = {
+        "text": "This is the home page",
+        "favorite_number": 31,
+        "list": [44,67,88,65,89]
+    }
+    return render(request, 'home.html', my_context)
 
 def contact_view(*args, **kwargs):
     return HttpResponse("<h1>Info</h1>")
